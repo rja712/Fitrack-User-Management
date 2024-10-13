d361ba7e30a9c67953fd0eff4f0541366aeb6288
Ankit Raj <rja.ankit@gmail.com>
2024-10-14 00:48:29 +0530

    init

---
+++ src/main/java/com/fitrack/userManagement/Fitrack/User/Management/FitrackUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.fitrack.userManagement.Fitrack.User.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitrackUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitrackUserManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitrack/userManagement/Fitrack/User/Management/FitrackUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitrack.userManagement.Fitrack.User.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitrackUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
